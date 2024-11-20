d1921356b4d89fca0e30212e7e5ca93bf6475e55
Lsy2006-github <lianyang12lol@gmail.com>
2024-11-20 09:16:56 +0800

    chore: added a test case for duplicated data

--- test/sheng-yang-backend.test.js
+++ test/sheng-yang-backend.test.js
@@ -34,22 +34,6 @@ describe("Resource API", () => {
           done();
         });
     });
-    it("should return 500 for input error", (done) => {
-      chai
-        .request(baseUrl)
-        .post("/add-game")
-        .send({
-          name: "",
-          price: "",
-          image: "",
-        })
-        .end((err, res) => {
-          expect(res).to.have.status(500);
-        //   expect(res.body.price).to.equal(undefined);
-        //   expect(res.body.message).to.equal(err.message);
-          done();
-        });
-    });
     it("Should add a new resource", (done) => {
       chai
         .request(baseUrl)
@@ -62,6 +46,22 @@ describe("Resource API", () => {
         .end((err, res) => {
           expect(res).to.have.status(201);
           expect(res.body).to.be.an("array");
+          done();
+        });
+    });
+    it("should return 400 for same data", (done) => {
+      chai
+        .request(baseUrl)
+        .post("/add-game")
+        .send({
+          name: "Test Resource 2",
+          price: "100",
+          image: "https://example.com/image.jpg",
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+        //   expect(res.body.price).to.equal(undefined);
+        //   expect(res.body.message).to.equal(err.message);
           done();
         });
     });
