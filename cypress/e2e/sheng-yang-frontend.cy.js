791847cdfb7181aec4b8c074070f9bae412b4728
Lsy2006-github <lianyang12lol@gmail.com>
2024-11-21 17:11:52 +0800

    chore: updated gitignore and frontend test case

--- cypress/e2e/sheng-yang-frontend.cy.js
+++ cypress/e2e/sheng-yang-frontend.cy.js
@@ -1,6 +1,6 @@
 describe("BeaconStudio Frontend", () => {
   let baseUrl;
-  before(() => {
+  beforeEach(() => {
     cy.task("startServer").then((url) => {
       baseUrl = url; // Store the base URL
       cy.visit(baseUrl + "/add-product.html"); // Visit the page
@@ -17,6 +17,20 @@ describe("BeaconStudio Frontend", () => {
   });
   it("should prompt with an alert without price filled", () => {
     cy.get("#name").type("Test frontend resource 1")
+    cy.get('button[type=submit]').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Name and Price are required!');
+    });
+  });
+  it("should prompt with an alert without name filled", () => {
+    cy.get("#price").type(100)
+    cy.get('button[type=submit]').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Name and Price are required!');
+    });
+  });
+  it("should prompt with an alert without name filled", () => {
+    cy.get("#price").type(100)
     cy.get('button[type=submit]').click();
     cy.on('window:alert', (str) => {
       expect(str).to.equal('Name and Price are required!');
